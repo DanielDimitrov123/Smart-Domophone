016db813abb55c33d80e424efc2fa5fd5954b658
Daniel Dimitrov <80510917+DanielDimitrov123@users.noreply.github.com>
2024-11-04 19:26:01 +0200

    Add files via upload

---
+++ SIM800L_CALL.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("ATD+8801201718869;");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
