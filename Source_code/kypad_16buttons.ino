6e8accfe579bce36fccb29882cf3298539f93239
Daniel Dimitrov <80510917+DanielDimitrov123@users.noreply.github.com>
2025-01-19 10:57:16 +0200

    Update kypad_16buttons.ino

--- Source_code/kypad_16buttons.ino
+++ Source_code/kypad_16buttons.ino
@@ -1,29 +1,38 @@
+int SCLpin = 13; 
+int SDOpin = 12;
+
+byte key;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  pinMode( SCLpin, OUTPUT);
+  pinMode( SDOpin, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  getKeyStatus();
-  delay(1000);
+  key = getKeyStatus();
+
+  if(key){
+    Serial.println(key);
+  }
+
+  delay(100);
 }
 
-int pinTouchData = 12; 
-int pinTouchClock = 13; 
+byte getKeyStatus() {
+  byte Count;
+  byte Key = 0;
 
-// key configuration
-uint8_t keys[16] = {0};
-
-// simple 8229 IC protocol, pinging the clock line 16 times to get all key states
-void getKeyStatus() {
-  uint8_t num = 0;
-  for (uint8_t i = 0; i < 16; i++) {
-    digitalWrite(pinTouchClock, LOW); // pull clock low
-    if (digitalRead(pinTouchData) == 0) // read bit for key state
-      keys[i] = 1; // key touched
-    else
-      keys[i] = 0; // not touched
-    digitalWrite(pinTouchClock, HIGH); // pull clock high to signal done
-  }   
+  for( Count = 1; Count <=16; Count++){
+    digitalWrite( SCLpin, LOW);
+    if( !digitalRead( SDOpin)){
+      Key = Count;
+    }
+    digitalWrite( SCLpin, HIGH);
+  } 
+  
+  return Key;
 }
